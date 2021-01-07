15fbd85692313913be444cf92b93c1e5ecc50562
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-07 11:06:19 +0530

    Added the Footer.js

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="p-2 bg-dark mt-5 mb-0">
+            <p className="lead text-center text-white mt-2">Developed by Pravunath Singh | Powered by GitHub Jobs API</p>
+        </footer>
+    )
+}
+
+export default Footer;
